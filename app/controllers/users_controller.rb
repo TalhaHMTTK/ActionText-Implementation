669e4aef5cf3fed340fa4df348f2cc71466fe409
TalhaHMTTK <talhahmttk2@gmail.com>
2024-06-20 16:07:59 +0500

    added delete functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,5 +29,8 @@ class UsersController < ApplicationController
         redirect_to @user
     end
 
-    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end
 end
